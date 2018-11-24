250a457e343101cd70cba1fa570f895f505ccf9d
mithun-Das <mithun.das227@gmail.com>
2018-11-24 19:18:10 +0600

    background image added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,ViewEncapsulation, OnInit } from '@angular/core';
 import { Post }  from './posts/post.model';
 import { AuthService } from './auth/auth.service';
 
@@ -6,7 +6,8 @@ import { AuthService } from './auth/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent implements OnInit {
   title = 'app';
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,14 +1,16 @@
 
-import { Component } from "@angular/core";
+import { Component,ViewEncapsulation,OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { AuthService } from "../auth.service";
 
 @Component({
     templateUrl : './login.component.html',
-    styleUrls : ['./login.component.css']
+    styleUrls : ['./login.component.css'],
+    encapsulation: ViewEncapsulation.None,
+
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
     constructor (private authService : AuthService) {
         
@@ -17,5 +19,7 @@ export class LoginComponent {
     onLogin(form : NgForm) {
         this.authService.login(form.value.email , form.value.password)
     }
+
+    ngOnInit(){}
 
 }
