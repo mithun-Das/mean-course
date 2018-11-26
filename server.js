04eb567746f67cda8ccda7307ddaf5c3a5500ce8
mithun-Das <mithun.das227@gmail.com>
2018-11-26 06:17:54 +0600

    angular setup

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use((req,res,next) => {
 });
 
 app.use(bodyParser.json());
-app.use(express.static(__dirname+'/dist/mean-course'));
+app.use(express.static(__dirname+'/dist'));
 app.use("/images", express.static(path.join("backend/images")));
 
 app.post("/login", userController.userLogin);
@@ -50,7 +50,7 @@ app.delete("/post/delete/:id", checkAuth, postController.deletePost);
 
 app.get("/login",(req,res) => {
 
-    res.sendFile(path.join(__dirname+'/dist/mean-course/login.html'));
+    res.sendFile(path.join(__dirname+'/dist/login.html'));
 });
 
 app.get("/test", (req,res,next) => {
