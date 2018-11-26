22ba7a146df0192abb40d85e27e26526317003b1
mithun-Das <mithun.das227@gmail.com>
2018-11-26 06:12:11 +0600

    angular setup

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use((req,res,next) => {
 });
 
 app.use(bodyParser.json());
-app.use(express.static(__dirname+'/dist'));
+app.use(express.static(__dirname+'/dist/mean-course'));
 app.use("/images", express.static(path.join("backend/images")));
 
 app.post("/login", userController.userLogin);
